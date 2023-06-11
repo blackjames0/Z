d8b429321ed8f30f451643c59d5a3d76d7af1d3d
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-06-11 16:16:21 +0600

    ok

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -43,8 +43,12 @@ from bot.helper.ext_utils.bulk_links import extract_bulk_links
 async def _mirror_leech(client, message, isQbit=False, isLeech=False, sameDir=None, bulk=[]):
     text = message.text.split('\n')
     input_list = text[0].split(' ')
+
+    arg_base = {'link': '', '-i': 0, '-m': '', '-d': False, '-j': False, '-s': False, '-b': False,
+                '-n': '', '-e': False, '-z': False, '-up': '', '-rcf': '', '-au': '', '-ap': ''}
+
     args = arg_parser(input_list[1:], arg_base)
-    arg_base = {'link': '', '-i': 0, '-m': '', '-d': False, '-j': False, '-s': False, '-b': False, '-n': '', '-e': False, '-z': False, '-up': '', '-rcf': '', '-au': '', '-ap': ''}
+
     try:
         multi = int(args['-i'])
     except:
